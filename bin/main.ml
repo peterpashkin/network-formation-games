d8d2e148b826599d29999164deecb7d25c886993
peterpashkin <pashkinpeter@gmail.com>
2025-12-02 01:03:09 +0100

    small changes

--- bin/main.ml
+++ bin/main.ml
@@ -11,8 +11,7 @@ let add_new_lines cnt =
     printf "\n"
   done
 
-let _sim_unilateral g =
-  let cost = 0.1 in
+let sim_unilateral g ~cost =
   let module G = Undirected_unilateral_game in
   let module N = UUNash in
   let first_cost = N.all_player_compute ~cost ~network:(Network.build g ~f:G.edge_formation) in
@@ -27,8 +26,7 @@ let _sim_unilateral g =
   let () = printf "Initial cost: %f, Final cost: %f\n" first_cost next_cost in
   ()
 
-let _sim_bilateral g =
-  let cost = 0.2 in
+let sim_bilateral g ~cost =
   let module G = Undirected_bilateral_game in
   let module N = UBNash in
 
@@ -41,3 +39,11 @@ let _sim_bilateral g =
   let () = N.pairwise_stability g ~cost |> (function Action.None -> true | _ -> false) |> printf "Iterated Graph is Nash: %b\n" in
   let () = printf "Initial cost: %f, Final cost: %f\n" first_cost next_cost in
   ()
+
+
+
+let cool_stars = Graph_gen.connected_stars ~sz:20 ~star_sz:5 ~alpha:1
+let cool_cliques = Graph_gen.sparsely_connected_cliques ~sz:20 ~c_sz:5 ~alpha:1
+
+let () = sim_unilateral cool_stars ~cost:1.5
+let() = sim_bilateral cool_cliques ~cost:2.
